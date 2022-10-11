585afefd463a7c5c831478606e61fae869d22112
Polly <pollylam1aab@gmail.com>
2022-10-11 18:53:20 +0800

    update path

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ import express, { application } from "express";
 import path from "path";
 let app = express();
 
-app.use(express.static(path.resolve("css")));
-app.use(express.static(path.resolve("js")));
-app.use(express.static(path.resolve("img")));
-app.use(express.static(path.resolve("fonts")));
-app.use(express.static(path.resolve("photo")));
+app.use(express.static(path.resolve("./css")));
+app.use(express.static(path.resolve("./js")));
+app.use(express.static(path.resolve("./img")));
+app.use(express.static(path.resolve("./fonts")));
+app.use(express.static(path.resolve("./photo")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./index.html"));
