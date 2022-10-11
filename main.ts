f91c2b2ef935463673f19577f76a6bd1db343d0f
Polly <pollylam1aab@gmail.com>
2022-10-11 17:22:25 +0800

    set simple server

---
+++ main.ts
@@ -0,0 +1,19 @@
+import express, { application } from "express";
+import path from "path";
+let app = express();
+
+app.use(express.static("/css"));
+app.use(express.static("/js"));
+app.use(express.static("/img"));
+app.use(express.static("/fonts"));
+app.use(express.static("/photo"));
+app.use(express.static("/.vscode"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./index.html"));
+});
+
+let port = 5501;
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
