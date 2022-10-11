570a065a261c6faf96d84452ce7174c9704dfdab
Polly <pollylam1aab@gmail.com>
2022-10-11 18:47:38 +0800

    update path

--- main.ts
+++ main.ts
@@ -7,9 +7,10 @@ app.use(express.static(path.resolve("js")));
 app.use(express.static(path.resolve("img")));
 app.use(express.static(path.resolve("fonts")));
 app.use(express.static(path.resolve("photo")));
-//app.get("/", (req, res) => {
-//  res.sendFile(path.resolve("./index.html"));
-//});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./index.html"));
+});
 
 let port = 5501;
 app.listen(port, () => {
