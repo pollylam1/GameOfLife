3ba4f2369491ea66648676e4e31ff6c07c3bc885
Polly <pollylam1aab@gmail.com>
2022-10-11 22:30:18 +0800

    fix-index.js

--- server.ts
+++ server.ts
@@ -1,16 +1,16 @@
-import express, { application } from "express";
+import express from "express";
 import path from "path";
 let app = express();
-
-app.use(express.static(path.resolve("css")));
-app.use(express.static(path.resolve("js")));
-app.use(express.static(path.resolve("img")));
-app.use(express.static(path.resolve("fonts")));
-app.use(express.static(path.resolve("photo")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./index.html"));
 });
+
+app.use(express.static("css"));
+app.use(express.static("js"));
+app.use(express.static("img"));
+app.use(express.static("fonts"));
+app.use(express.static("photo"));
 
 let port = 5501;
 app.listen(port, () => {
