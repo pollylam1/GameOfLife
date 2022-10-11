c63c6d9c01b4483cf7bb775bb0514da9e4c9850e
Polly <pollylam1aab@gmail.com>
2022-10-11 18:42:05 +0800

    update path

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import express, { application } from "express";
 import path from "path";
 let app = express();
 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./index.html"));
+});
+
 app.use(express.static("css"));
 app.use(express.static("js"));
 app.use(express.static("img"));
 app.use(express.static("fonts"));
 app.use(express.static("photo"));
 app.use(express.static(".vscode"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./index.html"));
-});
 
 let port = 5501;
 app.listen(port, () => {
